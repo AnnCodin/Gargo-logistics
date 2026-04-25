fc9e9724d4f985e6b3bb4581047dc9839545a1a5
AnnCodin <qantoinnette23@gmail.com>
2026-04-25 01:12:39 +0000

    Add a navbar with site logo

--- gargo-logistics/src/App.jsx
+++ gargo-logistics/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Topbar from "./Components/Topbar/Topbar.jsx";
+import Navbar from "./Components/Navbar/Navbar.jsx";
 
 const App = () => {
   return (
     <div>
       <Topbar />
+      <Navbar />
     </div>
   );
 };
---
+++ gargo-logistics/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Navbar.css";
+import site_logo from "../../assets/gargo_logo.png";
+
+const Navbar = () => {
+  return (
+    <nav className="container">
+      <img src={site_logo} alt="Site Logo" className="logo" />
+      <ul>
+        <li>Home</li>
+        <li>About Us</li>
+        <li>
+          Services <i class="ri-arrow-down-s-line"></i>
+        </li>
+        <li>
+          Pages <i class="ri-arrow-down-s-line"></i>
+        </li>
+        <li>
+          Blog <i class="ri-arrow-down-s-line"></i>
+        </li>
+        <li>Contact Us</li>
+      </ul>
+      <button>get a quote</button>
+    </nav>
+  );
+};
+
+export default Navbar;
