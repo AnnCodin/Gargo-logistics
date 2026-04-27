1f8a17c3998eb8b831adf55fb71f727742ce0550
AnnCodin <qantoinnette23@gmail.com>
2026-04-27 12:13:13 +0000

    Add main title and sub title

--- gargo-logistics/src/App.jsx
+++ gargo-logistics/src/App.jsx
@@ -3,6 +3,7 @@ import Topbar from "./Components/Topbar/Topbar.jsx";
 import Navbar from "./Components/Navbar/Navbar.jsx";
 import Hero from "./Components/Hero/Hero.jsx";
 import AboutUs from "./Components/About Us/AboutUs.jsx";
+import Title from "./Components/Title/Title.jsx";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <AboutUs />
+      <Title subTitle="our service" title="what we can do for you" />
     </div>
   );
 };
