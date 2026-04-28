125047ad431f0ef38481aaf398bdb684cbc1bd32
AnnCodin <qantoinnette23@gmail.com>
2026-04-28 19:02:41 +0000

    Embbed google map

--- gargo-logistics/src/Components/Contact/Contact.jsx
+++ gargo-logistics/src/Components/Contact/Contact.jsx
@@ -1,15 +1,14 @@
 import React from "react";
+import "./Contact.css";
 
 const Contact = () => {
   return (
-    <div className="location">
+    <div className="map-section">
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d204374.0215386538!2d-76.18826123892957!3d36.83423457662442!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1s99%20Roving%20St%2C%20Big%20City%2C%20PICU%2023454!5e0!3m2!1sen!2sgh!4v1777390201448!5m2!1sen!2sgh"
-        width="600"
-        height="450"
-        allowfullscreen=""
+        title="Google Map"
+        src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d25565.986330251828!2d-76.12249595124091!3d36.776605436901946!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1s99%20Roving%20St%2C%20Big%20City%2C%20PICU%2023454!5e0!3m2!1sen!2sgh!4v1777402108258!5m2!1sen!2sgh"
         loading="lazy"
-        referrerpolicy="no-referrer-when-downgrade"
+        allowFullScreen=""
       ></iframe>
     </div>
   );
